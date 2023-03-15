15067e89e44fea77894ee1dca4101244d6331387
Eduard Tarieladze <Edu@MacBook-Pro-Eduard.local>
2023-03-15 19:20:12 +0300

    commit 1

---
+++ task_1.py
@@ -0,0 +1,18 @@
+import requests
+from zipfile import ZipFile
+from io import BytesIO
+
+
+TOP_1M_DOMAINS = 'http://s3.amazonaws.com/alexa-static/top-1m.csv.zip'
+TOP_1M_DOMAINS_FILE = 'top-1m.csv'
+
+
+def get_data():
+    top_doms = requests(TOP_1M_DOMAINS, strem = True)
+    zipfile = ZipFile(BytesIO(top_doms.content))
+    top_data = zipfile.read(TOP_1M_DOMAINS_FILE).decode('utf-8')
+
+with open(TOP_1M_DOMAINS_FILE, 'w') as f:
+    f.write(top_data)
+
+
