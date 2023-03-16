9c84385b0a2aea25a31cf4bdf35e3c827cfe95de
Ed-Tari <estarieladze@edu.hse.ru>
2023-03-16 15:27:56 +0300

    Закончил скрипт, удалил jupyter file

--- task_1.py
+++ task_1.py
@@ -1,6 +1,12 @@
 import requests
 from zipfile import ZipFile
 from io import BytesIO
+import pandas as pd
+from datetime import timedelta
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
 
 
 TOP_1M_DOMAINS = 'http://s3.amazonaws.com/alexa-static/top-1m.csv.zip'
@@ -15,5 +21,39 @@ def get_data():
     with open(TOP_1M_DOMAINS_FILE, 'w') as f:
         f.write(top_data)
 
-print('Done')
 
+def get_stat():
+    top_data_df = pd.read_csv(TOP_1M_DOMAINS_FILE, name = ['rank', 'domain'])
+    top_data_10 = top_data_df[top_data_df['domain'].str.endwith('.ru') ]
+    top_data_10 = top_data_10.head(10)
+    with open('top_data_10.csv', 'w') as f:
+        f.write(top_data_10.to_csv(idnex = False, header = False))
+
+def print_data():
+    with open('top_data_10.csv', 'r') as f:
+        all_data = f.read()
+    print('Top domain in .RU')
+    print(all_data)
+
+default_args = {
+    'owner': 'a.batalov',
+    'depends_on_past': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5),
+    'start_date': datetime(2021, 10, 7),
+    'schedule_interval': '0 12 * * *'
+}
+dag = DAG('top_10_ru_new', default_args=default_args)
+
+
+t1 = PythonOperator(task_id = 'get_data',
+                    python_callable=get_data, 
+                    dag = dag) 
+t2 = PythonOperator(task_id = 'get_tat',
+                    python_callable=get_stat,
+                    dag = dag)
+t3 = PythonOperator(task_id = 'print_data',
+                    python_callable= print_data,
+                    dag = dag)
+
+t1 >> t2 >> t3
