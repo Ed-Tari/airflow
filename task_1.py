be51850c67472a4c5ded4de05c03c646d7081d62
Ed-Tari <estarieladze@edu.hse.ru>
2023-03-16 16:17:39 +0300

    add top_domains .com

--- task_1.py
+++ task_1.py
@@ -29,11 +29,23 @@ def get_stat():
     with open('top_data_10.csv', 'w') as f:
         f.write(top_data_10.to_csv(index=  False, header = False))
 
+def get_stat_com():
+    top_data_df = pd.read_csv(TOP_1M_DOMAINS_FILE,  names = ['rank', 'domain'])
+    top_data_10 = top_data_df[top_data_df['domain'].str.endswith('.com') ]
+    top_data_10 = top_data_10.head(10)
+    with open('top_data_10_com.csv', 'w') as f:
+        f.write(top_data_10.to_csv(index=  False, header = False))
+
+
 def print_data():
     with open('top_data_10.csv', 'r') as f:
         all_data = f.read()
+    with open('top_data_10_com.csv', 'r') as f:
+        all_data_com = f.read()
     print('Top domain in .RU')
     print(all_data)
+    print('Top domain in .COM')
+    print(all_data_com)
 
 default_args = {
     'owner': 'a.batalov',
@@ -49,15 +61,14 @@ dag = DAG('top_10_ru_new', default_args=default_args)
 t1 = PythonOperator(task_id = 'get_data',
                     python_callable=get_data, 
                     dag = dag) 
-t2 = PythonOperator(task_id = 'get_tat',
+t2 = PythonOperator(task_id = 'get_stat',
                     python_callable=get_stat,
+                    dag = dag)
+t2_com = PythonOperator(task_id = 'get_stat_com',
+                    python_callable=get_stat_com,
                     dag = dag)
 t3 = PythonOperator(task_id = 'print_data',
                     python_callable= print_data,
                     dag = dag)
 
-t1 >> t2 >> t3
-
-get_data()
-get_stat()
-print_data()
+t1 >> [t2, t2_com]>> t3
